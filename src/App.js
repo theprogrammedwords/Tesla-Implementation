b986e06d54a31d4695515314f39605410b4ca300
Ashish Prasad <rprasad43255@gmail.com>
2021-09-21 01:10:57 +0530

    ADD : Redux for Cars

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-      {/* <Header/> */}
-      <Home/>
+      <Header />
+      <Home />
     </div>
   );
 }
